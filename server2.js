ff6a032266c74741040ac953471e8b9dc5520407
NIBEDITA-JHA <121035319+NIBEDITA-JHA@users.noreply.github.com>
2024-06-26 16:01:16 +0530

    Delete server2.js

--- server2.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app= express();
-app.use(bodyparser.json());
-const port = 8000;
-app.listen(port, () => console.log(`App started at port${port}`));
